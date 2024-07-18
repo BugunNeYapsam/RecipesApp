03cc160be8ff43266ef19d23f108a54acc7f34cd
KadirBilen <kdrbln17@gmail.com>
2024-07-18 18:41:35 +0300

    test değişiklik

--- Components/Categories.js
+++ Components/Categories.js
@@ -1,3 +1,5 @@
+// TEST TEST TEST
+
 import React from 'react';
 import { View, ScrollView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
