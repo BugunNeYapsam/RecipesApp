fda0854fae208abc455af713991fe590552973a1
BurhanBilen <burhan1374@hotmail.com>
2024-10-04 11:43:59 +0300

    splash gözükmesi

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AppProvider } from './Context/AppContext';
+import * as SplashScreen from 'expo-splash-screen';
 import Main from './Main';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  useEffect(() => {
+    async function prepare() {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      SplashScreen.hideAsync();
+    }
+
+    prepare();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AppProvider>
